220880f3c998f94f265884cdab647414e2fddf74
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2020-03-13 10:15:19 +0100

    Modul/Lab 09: WinForms + Events

---
+++ ButtonMovement/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ButtonMovement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLeft = new System.Windows.Forms.Button();
+            this.BtnRight = new System.Windows.Forms.Button();
+            this.BtnMain = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnLeft
+            // 
+            this.BtnLeft.Location = new System.Drawing.Point(12, 12);
+            this.BtnLeft.Name = "BtnLeft";
+            this.BtnLeft.Size = new System.Drawing.Size(103, 66);
+            this.BtnLeft.TabIndex = 0;
+            this.BtnLeft.Text = "Links";
+            this.BtnLeft.UseVisualStyleBackColor = true;
+            this.BtnLeft.Click += new System.EventHandler(this.BtnLeft_Click);
+            // 
+            // BtnRight
+            // 
+            this.BtnRight.Location = new System.Drawing.Point(685, 12);
+            this.BtnRight.Name = "BtnRight";
+            this.BtnRight.Size = new System.Drawing.Size(103, 66);
+            this.BtnRight.TabIndex = 1;
+            this.BtnRight.Text = "Rechts";
+            this.BtnRight.UseVisualStyleBackColor = true;
+            this.BtnRight.Click += new System.EventHandler(this.BtnRight_Click);
+            // 
+            // BtnMain
+            // 
+            this.BtnMain.Location = new System.Drawing.Point(12, 84);
+            this.BtnMain.Name = "BtnMain";
+            this.BtnMain.Size = new System.Drawing.Size(776, 28);
+            this.BtnMain.TabIndex = 2;
+            this.BtnMain.Text = "Start";
+            this.BtnMain.UseVisualStyleBackColor = true;
+            this.BtnMain.Click += new System.EventHandler(this.BtnMain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 124);
+            this.Controls.Add(this.BtnMain);
+            this.Controls.Add(this.BtnRight);
+            this.Controls.Add(this.BtnLeft);
+            this.Name = "Form1";
+            this.Text = "Bewegte Buttons";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnLeft;
+        private System.Windows.Forms.Button BtnRight;
+        private System.Windows.Forms.Button BtnMain;
+    }
+}
+
---
+++ ButtonMovement/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonMovement
+{
+    public partial class Form1 : Form
+    {
+        //Properties zum Zwischenspeichern der Startpositionswerte
+        public int BtnLeft_Start { get; private set; }
+        public int BtnRight_Start { get; private set; }
+
+        //Fenster-Konstruktor
+        public Form1()
+        {
+            //Methode zum Erstellen der im Desigener definierten Elemente
+            InitializeComponent();
+
+            //Zwischenspeichern der Startwerte
+            BtnLeft_Start = BtnLeft.Left;
+            BtnRight_Start = BtnRight.Left;
+
+            //Anmelden der Methoden am Click-Event des dritten Buttons
+            BtnMain.Click += BtnLeft_Click;
+            BtnMain.Click += BtnRight_Click;
+            BtnMain.Click += BtnMain_Click;
+        }
+
+        //Event-Handler-Methode des linken Buttons
+        private void BtnLeft_Click(object sender, EventArgs e)
+        {
+            //Bewegen des linken Buttons um 10 Pixel nach rechts
+            BtnLeft.Left += 10;
+        }
+
+        //Event-Handler-Methode des rechten Buttons
+        private void BtnRight_Click(object sender, EventArgs e)
+        {
+            //Bewegen des rechten Buttons um 10 Pixel nach links
+            BtnRight.Left -= 10;
+        }
+
+        //Weitere Event-Handler-Methode
+
+        private void BtnMain_Click(object sender, EventArgs e)
+        {
+            //Aufruf der anderen EventHandler
+            //BtnLeft_Click(sender, e);
+            //BtnRight_Click(sender, e);
+
+            //Prüfung, ob die Buttons sich überlagern
+            if (BtnLeft.Right >= BtnRight.Left)
+                //Messagebox-Aufruf mit Abfrage, ob die Buttons zurückgesetzt werden sollen
+                if (MessageBox.Show("Die Buttons berühren sich!\nZurücksetzen?", "Berührung", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                {
+                    //Belegung der Buttonspositionen mit den gespeicherten Startwerten
+                    BtnLeft.Left = BtnLeft_Start;
+                    BtnRight.Left = BtnRight_Start;
+                }
+        }
+
+    }
+}
